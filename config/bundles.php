fd348eb9ab0692aead2a928219c408b6a721fccc
Geoffrey Hervet <GeoffreyHervet@users.noreply.github.com>
2018-02-20 15:53:21 +0100

    Add RabbitMQ (#3)

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
 ];
